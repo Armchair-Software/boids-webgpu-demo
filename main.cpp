723e02e593acade52181c695161b923c6db397cf
SlowRiot <slowriot@voxelstorm.com>
2024-12-09 18:44:09 +0000

    moar boids in release mode (2000 vs 500 in debug)

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@
 #include "render/webgpu_renderer.h"
 
 class game_manager {
-  flockstorm::manager boids{500};
+  #ifdef NDEBUG
+    flockstorm::manager boids{2000};
+  #else
+    flockstorm::manager boids{500};
+  #endif // NDEBUG
 
   logstorm::manager logger{logstorm::manager::build_with_sink<logstorm::sink::console>()}; // logging system
   render::webgpu_renderer renderer{logger, boids.num_boids};                    // WebGPU rendering system
